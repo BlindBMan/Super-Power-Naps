4111445d41602072e98f5e7d83b41203d4cb2a46
Samuel Gustin <gustin.daniel1@gmail.com>
2020-03-11 02:06:21 +0200

    Alarm layout + nap btn
    Added alarm activity and layout
    alarm_options_fragment contains buttons for different types of alarms
    Connected nap button with MainActivity using interface

--- Super-Power-Naps/app/src/main/java/com/example/super_power_naps/TakeANapFragment.java
+++ Super-Power-Naps/app/src/main/java/com/example/super_power_naps/TakeANapFragment.java
@@ -5,16 +5,50 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class TakeANapFragment extends Fragment {
+    private OnTakeNapClickListener activ_listener;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_take_anap,  container, false);
+        View view =  inflater.inflate(R.layout.fragment_take_anap,  container, false);
+        Button nap_btn = view.findViewById(R.id.nap_btn);
+
+        nap_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activ_listener.onNapBtnClick();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof OnTakeNapClickListener) {
+            activ_listener = (OnTakeNapClickListener) context;
+        } else {
+            throw  new RuntimeException(context.toString() + "must implement  OnTakeNapClickListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activ_listener = null;
+    }
+
+    public interface OnTakeNapClickListener {
+        void onNapBtnClick();
     }
 }
